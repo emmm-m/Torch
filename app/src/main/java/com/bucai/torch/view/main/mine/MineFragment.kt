d29bf1d0881daac176507e5ccb078e7ffde4b586
zx-Zhu <354328995@qq.com>
2018-06-26 21:40:20 +0800

    mine

--- app/src/main/java/com/bucai/torch/view/main/mine/MineFragment.kt
+++ app/src/main/java/com/bucai/torch/view/main/mine/MineFragment.kt
@@ -6,17 +6,12 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.avos.avoscloud.*
+import com.avos.avoscloud.AVFile
+import com.avos.avoscloud.AVUser
 import com.bucai.torch.R
-import com.bucai.torch.bean.Comment
-import com.bucai.torch.bean.Lecturer
-import com.bucai.torch.util.LogUtil
-import com.bucai.torch.util.leancloud.GetDataModel
 import com.bucai.torch.view.login.LoginActivity
-import com.google.gson.Gson
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.fragment_mine.*
-import java.util.*
-import kotlin.collections.ArrayList
 
 /**
  * 我的
@@ -32,69 +27,17 @@ class MineFragment : Fragment() {
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        mine_send.setOnClickListener {
-            val simpleComment = Comment()
-            simpleComment.comment = "评论啦啦啦啦"
-            simpleComment.effectRatio = 100
-            simpleComment.serveRatio = 100
-            simpleComment.good = 10
-            simpleComment.totalComment = 20
-            simpleComment.time = Date().time.toString()
-            simpleComment.userObjectId = "5b29e75aac502e0031a66166"
-            val completeComment = ArrayList<Comment>()
-            completeComment.add(simpleComment)
-            completeComment.add(simpleComment)
-            completeComment.add(simpleComment)
-            val gson = Gson()
-            val avObject = AVObject("Lecturer")
-            avObject.put("description", arrayOf("帅气迷人", "文武双全", "金枪不倒", "善解人衣"))
-            avObject.put("commentGroup", arrayOf("讲课认真19", "耐心16", "思维敏捷12"))
-            avObject.put("studentCount", 5)
-            avObject.put("simpleIntroduce", "教龄8年 | 已授536课")
-            avObject.put("successCase", arrayOf("朱展萱同学 中考成绩750\n朱展萱特别优秀", "朱展萱同学 中考成绩750\n朱展萱特别优秀"))
-            avObject.put("experience", "教学100余年，老字号")
-            avObject.put("star", 5)
-            avObject.put("teaName", "Mr.Li")
-            avObject.put("completeIntroduce", "这是一条很长很长的完整介绍")
-            avObject.put("simpleComment", gson.toJson(simpleComment))
-            avObject.put("goodCommentCount", 20)
-            avObject.put("location", AVGeoPoint(1.0, 1.0))
-            avObject.put("price", 100)
-            avObject.put("completeComment", gson.toJson(completeComment))
-            avObject.saveInBackground(object : SaveCallback() {
-                override fun done(p0: AVException?) {
-                    p0?.printStackTrace()
-                    if (p0 != null)
-                        LogUtil.e("push success")
-                }
-            })
+        if (AVUser.getCurrentUser().get("header") != null) {
+            val header = AVUser.getCurrentUser().get("header") as AVFile
+            Glide.with(context).load(header.url).crossFade().into(header_mine)
+        } else {
+            header_mine.setImageResource(R.drawable.ic_header_default)
         }
+        name_mine.text = AVUser.getCurrentUser().get("nickname").toString()
 
-        mine_get.setOnClickListener {
-            val model = GetDataModel()
-            model.getLecturerList(object : GetDataModel.GetDataListener<Lecturer> {
-                override fun onStart() {
-
-                }
-
-                override fun onError(e: AVException?) {
-                    e?.printStackTrace()
-                }
-
-                override fun onFinish(list: MutableList<Lecturer>?) {
-                    list?.forEach {
-                        var lecturer = model.getLectureDetail(it)
-                        lecturer = model.getLecturerComment(lecturer)
-                        LogUtil.e(lecturer.toString())
-                    }
-                }
-
-            })
-        }
         btn_logout.setOnClickListener {
             AVUser.logOut()
-            startActivity(Intent(activity, LoginActivity::class.java))
+            startActivity(Intent(context, LoginActivity::class.java))
             activity!!.finish()
         }
     }
